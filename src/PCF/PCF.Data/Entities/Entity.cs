6d5ceb6ac4b7e2f75f7bf2a9748e5fdc1bc47590
Filipe Alan <filipealan@gmail.com>
2025-01-14 21:09:02 -0300

    Delete Entity.cs

--- src/PCF/PCF.Data/Entities/Entity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PCF.Data.Entities
-{
-    public abstract class Entity
-    {
-        public int Id { get; set; }
-        public DateTime CriadoEm { get; private set; } = DateTime.UtcNow;
-        public DateTime? ModificadoEm { get; set; } = DateTime.UtcNow;
-    }
-}
