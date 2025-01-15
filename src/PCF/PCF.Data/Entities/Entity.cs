5f83ed881a4f3837041a2ae16e0ecfef929d9c53
Filipe Alan <filipealan@gmail.com>
2025-01-14 21:05:11 -0300

    Create Entity.cs

---
+++ src/PCF/PCF.Data/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace PCF.Data.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CriadoEm { get; private set; } = DateTime.UtcNow;
+        public DateTime? ModificadoEm { get; set; } = DateTime.UtcNow;
+    }
+}
