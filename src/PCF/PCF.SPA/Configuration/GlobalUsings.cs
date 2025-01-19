2fb9f157b813f33fa4d5558bc5b07b0fe45b541f
Jonatas Cruz <jonataspc@gmail.com>
2025-01-19 18:29:23 -0300

    feat: ajustes UI e autenticação

---
+++ src/PCF/PCF.SPA/Configuration/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using PCF.Shared.Dtos;
--- src/PCF/PCF.SPA/Services/AuthService.cs
+++ src/PCF/PCF.SPA/Services/AuthService.cs
@@ -1,96 +1,101 @@
-﻿    using Blazored.LocalStorage;
-    using PCF.SPA.Dto;
-    using System.Net.Http.Json;
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Json;
 
-    namespace PCF.SPA.Services
+namespace PCF.SPA.Services
+{
+    public class AuthService
     {
-        public class AuthService
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        // Evento para notificar mudanças no estado de autenticação
+        public event Action? OnAuthenticationStateChanged;
+
+        public AuthService(HttpClient httpClient, ILocalStorageService localStorage)
         {
-            private readonly HttpClient _httpClient;
-            private readonly ILocalStorageService _localStorage;
-
-            // Evento para notificar mudanças no estado de autenticação
-            public event Action? OnAuthenticationStateChanged;
-
-            public AuthService(HttpClient httpClient, ILocalStorageService localStorage)
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+        public async Task<bool> LoginAsync(LoginResponseDto loginResponseDto)
+        {
+            try
             {
-                _httpClient = httpClient;
-                _localStorage = localStorage;
-            }
-            public async Task<bool> LoginAsync(LoginResponseDto loginResponseDto)
-            {
-                try
+                var response = await _httpClient.PostAsJsonAsync("auth/login", loginResponseDto);
+                if (response.IsSuccessStatusCode)
                 {
-                    var response = await _httpClient.PostAsJsonAsync("auth/login", loginResponseDto);
-                    if (response.IsSuccessStatusCode)
+                    var token = await response.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrWhiteSpace(token))
                     {
-                        var token = await response.Content.ReadAsStringAsync();
-                        if (!string.IsNullOrWhiteSpace(token))
-                        {
-                            await _localStorage.SetItemAsync("authToken", token);
-                            _httpClient.DefaultRequestHeaders.Authorization =
-                                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                        await _localStorage.SetItemAsync("authToken", token);
+                        _httpClient.DefaultRequestHeaders.Authorization =
+                            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
-                            // Notifica a mudança de estado de autenticação
-                            NotifyAuthenticationStateChanged();
-                            return true;
-                        }
+                        // Notifica a mudança de estado de autenticação
+                        NotifyAuthenticationStateChanged();
+                        return true;
                     }
-                    return false;
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erro no login: {ex.Message}");
-                    return false;
-                }
+                return false;
             }
-            public async Task LogoutAsync()
+            catch (Exception ex)
             {
-                try
-                {
-                    await _localStorage.RemoveItemAsync("authToken");
-                    _httpClient.DefaultRequestHeaders.Authorization = null;
+                Console.WriteLine($"Erro no login: {ex.Message}");
+                return false;
+            }
+        }
+        public async Task LogoutAsync()
+        {
+            try
+            {
+                await _localStorage.RemoveItemAsync("authToken");
+                _httpClient.DefaultRequestHeaders.Authorization = null;
 
-                    // Notifica a mudança de estado de autenticação
-                    NotifyAuthenticationStateChanged();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erro ao realizar logout: {ex.Message}");
-                    throw; // Re-lança a exceção para ser tratada em um nível superior, se necessário
-                }
+                // Notifica a mudança de estado de autenticação
+                NotifyAuthenticationStateChanged();
             }
-            public async Task<string> GetTokenAsync()
+            catch (Exception ex)
             {
-                return await _localStorage.GetItemAsync<string>("authToken");
+                Console.WriteLine($"Erro ao realizar logout: {ex.Message}");
+                throw; // Re-lança a exceção para ser tratada em um nível superior, se necessário
             }
-            public async Task<string> RegisterAsync(LoginResponseDto loginResponseDto)
+        }
+        public async Task<string?> GetTokenAsync()
+        {
+            return await _localStorage.GetItemAsync<string>("authToken");
+        }
+
+        public async Task<bool> IsLoggedIn()
+        {
+            return !string.IsNullOrEmpty(await GetTokenAsync());
+        }
+
+        public async Task<string> RegisterAsync(LoginResponseDto loginResponseDto)
+        {
+            try
             {
-                try
+                var response = await _httpClient.PostAsJsonAsync("auth/register", loginResponseDto);
+                if (response.IsSuccessStatusCode)
                 {
-                    var response = await _httpClient.PostAsJsonAsync("auth/register", loginResponseDto);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "Registrado com sucesso";
-                    }
+                    return "Registrado com sucesso";
+                }
                 // Captura a mensagem de erro retornada pelo servidor
                 var errorMessage = await response.Content.ReadAsStringAsync();
                 return string.IsNullOrWhiteSpace(errorMessage)
                     ? "Falha ao registrar usuário."
                     : errorMessage;
             }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erro ao registrar usuário: {ex.Message}");
-                    return "Erro ao registrar usuário. Tente novamente mais tarde.";
-                }
-            }
-            // Método para notificar a mudança de estado de autenticação
-            private void NotifyAuthenticationStateChanged()
+            catch (Exception ex)
             {
-                OnAuthenticationStateChanged?.Invoke();
+                Console.WriteLine($"Erro ao registrar usuário: {ex.Message}");
+                return "Erro ao registrar usuário. Tente novamente mais tarde.";
             }
         }
+        // Método para notificar a mudança de estado de autenticação
+        private void NotifyAuthenticationStateChanged()
+        {
+            OnAuthenticationStateChanged?.Invoke();
+        }
     }
+}
 
 
