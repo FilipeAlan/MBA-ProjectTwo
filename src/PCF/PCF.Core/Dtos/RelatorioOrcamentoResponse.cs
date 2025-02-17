34afe7a974fdf86a31bbafed5aa1caf38ac3d8c2
Jonatas Cruz <jonataspc@gmail.com>
2025-02-17 20:39:49 -0300

    refact: ajuste warnings

--- src/PCF/PCF.Core/Dtos/RelatorioOrcamentoResponse.cs
+++ src/PCF/PCF.Core/Dtos/RelatorioOrcamentoResponse.cs
@@ -4,14 +4,14 @@
     {
         public int TransacaoId { get; set; }
         public DateTime DataLancamento { get; set; }
-        public string TipoLancamento { get; set; }
+        public required string TipoLancamento { get; set; }
         public decimal Valor { get; set; }
         public decimal ValorLimite { get; set; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public int CategoriaId { get; set; }
-        public string Categoria { get; set; }
-        public string Tipo { get; set; }
+        public required string Categoria { get; set; }
+        public required string Tipo { get; set; }
         public int UsuarioId { get; set; }
-        public string Usuario { get; set; }
+        public required string Usuario { get; set; }
     }
 }
