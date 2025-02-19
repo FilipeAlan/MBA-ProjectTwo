3390cebf4c4d1c4ef48c547652f1bd16b18e003d
Eduardo Gimenes <dugimenes@gmail.com>
2025-02-19 12:19:27 -0300

    Ajuste construtor.

--- src/PCF/PCF.Core/Dtos/Login/RegisterResponse.cs
+++ src/PCF/PCF.Core/Dtos/Login/RegisterResponse.cs
@@ -2,9 +2,9 @@
 {
     public class RegisterResponse
     {
-        public string Name { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public string? ConfirmedPassword { get; set; }
     }
 }
